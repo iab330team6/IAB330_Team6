12ae42a4f0d893e973987ca01399c9bd1af77c96
Jordan Sy <n9971386@qut.edu.au>
2019-09-12 12:22:26 +1000

    test

--- LifeMate/LifeMate/Views/MenuPage.xaml.cs
+++ LifeMate/LifeMate/Views/MenuPage.xaml.cs
@@ -23,8 +23,7 @@ namespace LifeMate.Views
                 new HomeMenuItem {Id = MenuItemType.Browse, Title="Browse" },
                 new HomeMenuItem {Id = MenuItemType.About, Title="About" },
                 new HomeMenuItem {Id = MenuItemType.About, Title="Profile" },
-                new HomeMenuItem {Id = MenuItemType.About, Title="Updated?" },
-                new HomeMenuItem {Id = MenuItemType.About, Title="next?" }
+                new HomeMenuItem {Id = MenuItemType.About, Title="Updated?" }
             };
 
             ListViewMenu.ItemsSource = menuItems;
