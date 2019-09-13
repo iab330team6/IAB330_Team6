b9b4755eaa63a18a5c89300371bfb8659340a8de
jsy1910 <50371783+jsy1910@users.noreply.github.com>
2019-09-13 23:59:12 +1000

    Update on everything so far

---
+++ HomeMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeMate.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About,
+        Calendar,
+        Profile
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
