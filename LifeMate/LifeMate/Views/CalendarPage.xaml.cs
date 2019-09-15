15739a1dfed09f21d930b53230aff7b50576cdad
jsy1910 <50371783+jsy1910@users.noreply.github.com>
2019-09-15 15:24:25 +1000

    Updated/Cleaned up code

--- LifeMate/LifeMate/Views/CalendarPage.xaml.cs
+++ LifeMate/LifeMate/Views/CalendarPage.xaml.cs
@@ -25,147 +25,31 @@ namespace LifeMate.Views
         }
 
         private List<String> months = new List<String>() { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
-
-        
+        int i = 0;
 
         private void CreateGrid()
         {
             // Tried Assigning strings from list into the created labels from grid. Not sure how to call it exactly
 
-            //for (int i = 0; i < months.Count; i++)
-            //{
-            //    for (int rowindex = 0; rowindex < 3; rowindex++)
-            //    {
-            //        grid.RowDefinitions.Add(new RowDefinition() { Height = 100 });
-            //        for (int columnindex = 0; columnindex < 4; columnindex++)
-            //        {
-            //            foreach (string v in months)
-            //            {
-            //                Label label = new Label()
-            //                {
-            //                    Text = months[i],
-            //                    BackgroundColor = Color.WhiteSmoke,
-            //                    HorizontalOptions = LayoutOptions.FillAndExpand,
-            //                    VerticalOptions = LayoutOptions.FillAndExpand,
-            //                    VerticalTextAlignment = TextAlignment.Start,
-            //                    HorizontalTextAlignment = TextAlignment.Start
-            //                };
-            //                grid.Children.Add(label, columnindex, rowindex);
-            //            }
+            Label yearViewer = new Label()
+            {
+                Text = "2019",
+                BackgroundColor = Color.WhiteSmoke,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
 
-            //        }
-            //    }
-
-            //}
-
-
-            // Defining Grids
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-
-            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
-            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
-            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
-            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
-
-            // Months Labels
-            var january = new Label
-            { Text = "JAN",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var february = new Label
-            { Text = "FEB",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var march = new Label
-            { Text = "MAR",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var april = new Label
-            { Text = "APR",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var may = new Label
-            { Text = "MAY",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var june = new Label
-            { Text = "JUN",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var july = new Label
-            { Text = "JUL",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var august = new Label
-            { Text = "AUG",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var september = new Label
-            { Text = "SEP",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var october = new Label
-            { Text = "OCT",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var november = new Label
-            { Text = "NOV",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
-
-            var december = new Label
-            { Text = "DEC",
-            BackgroundColor = Color.WhiteSmoke,
-            HorizontalOptions = LayoutOptions.FillAndExpand,
-            VerticalOptions = LayoutOptions.FillAndExpand,
-            VerticalTextAlignment = TextAlignment.Start,
-            HorizontalTextAlignment = TextAlignment.Start};
+            Label blank = new Label()
+            {
+                Text = " ",
+                BackgroundColor = Color.White,
+                HorizontalOptions = LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.Center,
+                VerticalTextAlignment = TextAlignment.Start,
+                HorizontalTextAlignment = TextAlignment.Start
+            };
 
             Button profilePicHolder = new Button()
             {
@@ -174,7 +58,8 @@ namespace LifeMate.Views
                 HeightRequest = 100,
                 HorizontalOptions = Xamarin.Forms.LayoutOptions.Center,
                 VerticalOptions = Xamarin.Forms.LayoutOptions.Center,
-                BackgroundColor = Color.WhiteSmoke};
+                BackgroundColor = Color.WhiteSmoke
+            };
 
 
             Button birthdays = new Button()
@@ -184,7 +69,8 @@ namespace LifeMate.Views
                 HeightRequest = 50,
                 HorizontalOptions = Xamarin.Forms.LayoutOptions.Center,
                 VerticalOptions = Xamarin.Forms.LayoutOptions.Center,
-                BackgroundColor = Color.WhiteSmoke};
+                BackgroundColor = Color.WhiteSmoke
+            };
 
             Button exercise = new Button()
             {
@@ -193,7 +79,8 @@ namespace LifeMate.Views
                 HeightRequest = 50,
                 HorizontalOptions = Xamarin.Forms.LayoutOptions.Center,
                 VerticalOptions = Xamarin.Forms.LayoutOptions.Center,
-                BackgroundColor = Color.WhiteSmoke};
+                BackgroundColor = Color.WhiteSmoke
+            };
 
             Button alarm = new Button()
             {
@@ -202,7 +89,8 @@ namespace LifeMate.Views
                 HeightRequest = 50,
                 HorizontalOptions = Xamarin.Forms.LayoutOptions.Center,
                 VerticalOptions = Xamarin.Forms.LayoutOptions.Center,
-                BackgroundColor = Color.WhiteSmoke};
+                BackgroundColor = Color.WhiteSmoke
+            };
 
             Button work = new Button()
             {
@@ -211,7 +99,8 @@ namespace LifeMate.Views
                 HeightRequest = 50,
                 HorizontalOptions = Xamarin.Forms.LayoutOptions.Center,
                 VerticalOptions = Xamarin.Forms.LayoutOptions.Center,
-                BackgroundColor = Color.WhiteSmoke};
+                BackgroundColor = Color.WhiteSmoke
+            };
 
             Label viewer = new Label()
             {
@@ -220,28 +109,46 @@ namespace LifeMate.Views
                 HorizontalOptions = LayoutOptions.Start,
                 VerticalOptions = LayoutOptions.Center,
                 VerticalTextAlignment = TextAlignment.Start,
-                HorizontalTextAlignment = TextAlignment.Start};
+                HorizontalTextAlignment = TextAlignment.Start
+            };
 
-            // Months into grid
-            grid.Children.Add(january, 0, 0);
-            grid.Children.Add(february, 1, 0);
-            grid.Children.Add(march, 2, 0);
-            grid.Children.Add(april, 3, 0);
-            grid.Children.Add(may, 0, 1);
-            grid.Children.Add(june, 1, 1);
-            grid.Children.Add(july, 2, 1);
-            grid.Children.Add(august, 3, 1);
-            grid.Children.Add(september, 0, 2);
-            grid.Children.Add(october, 1, 2);
-            grid.Children.Add(november, 2, 2);
-            grid.Children.Add(december, 3, 2);
-            grid.Children.Add(profilePicHolder, 0, 3);
-            grid.Children.Add(viewer, 1, 3, 3, 4);
-            grid.Children.Add(birthdays, 0, 4);
-            grid.Children.Add(exercise, 0, 5);
-            grid.Children.Add(alarm, 0, 6);
-            grid.Children.Add(work, 0, 7);
+            grid.Children.Add(yearViewer, 0, 0);
+            grid.Children.Add(blank, 0, 4);
+            
 
+            for (int rowindex = 0; rowindex < 3; rowindex++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition() { Height = 100 });
+                for (int columnindex = 0; columnindex < 4; columnindex++)
+                {
+                    Label label = new Label()
+                    {
+                        Text = months[i],
+                        BackgroundColor = Color.WhiteSmoke,
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        VerticalTextAlignment = TextAlignment.Start,
+                        HorizontalTextAlignment = TextAlignment.Start
+                    };
+                    grid.Children.Add(label, columnindex, rowindex + 1);
+                    i++;
+                }
+            }
+
+
+            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
+            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
+            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
+            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
+            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
+            grid.RowDefinitions.Add(new RowDefinition { Height = 50 });
+
+            grid.Children.Add(profilePicHolder, 0, 5);
+            grid.Children.Add(viewer, 1, 6, 3, 8);
+            grid.Children.Add(birthdays, 0, 6);
+            grid.Children.Add(exercise, 0, 7);
+            grid.Children.Add(alarm, 0, 8);
+            grid.Children.Add(work, 0, 9);
 
             // Grid ScrollView
             calendarScrollView.Content = grid;
