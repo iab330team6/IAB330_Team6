658d1b91ca87a53b2052665d420452501d8bb138
jsy1910 <50371783+jsy1910@users.noreply.github.com>
2019-09-19 11:50:07 +1000

    Implementing Syncfusion Calendar API

---
+++ LifeMate/LifeMate/ViewModels/CalendarTestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeMate.ViewModels
+{
+    public class CalendarTestViewModel : BaseViewModel
+    {
+        public CalendarTestViewModel()
+        {
+            Title = "CalendarTest";
+        }
+    }
+}
