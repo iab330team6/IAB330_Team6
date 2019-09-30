7fd364bd77fd4e309ca3fe4f320d454248298639
MatthewMcGowan <n10025634@qut.edu.au>
2019-09-30 15:12:10 +1000

    Began Implementing Login/Signup Pages. UI is in, functionality to be add

---
+++ LifeMate/LifeMate/Views/LoginPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace LifeMate.Views
+{
+    public class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            this.Title = "Welcome to LifeMate!";
+            StackLayout stackLayout = new StackLayout();
+            Button btnLogin = new Button();
+            btnLogin.Text = "Login";
+            Button btnSignup = new Button();
+            btnSignup.Text = "Signup";
+            stackLayout.Children.Add(btnLogin);
+            stackLayout.Children.Add(btnSignup);
+
+            Content = stackLayout;
+
+        }
+
+        private async void btnSignup_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignupPage());
+        }
+    }
+}
---
+++ LifeMate/LifeMate/Views/SignupPage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace LifeMate.Views
+{
+    public class SignupPage : ContentPage
+    {
+        private Entry userEntry;
+        private Entry passwordEntry;
+        private Button createBtn;
+
+        string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "lifeMateDB.db");
+
+        public SignupPage()
+        {
+            this.Title = "Create An Account!";
+
+            StackLayout stackLayout = new StackLayout();
+
+            userEntry = new Entry();
+            userEntry.Keyboard = Keyboard.Text;
+            userEntry.Placeholder = "Username";
+            stackLayout.Children.Add(userEntry);
+
+            passwordEntry = new Entry();
+            passwordEntry.Keyboard = Keyboard.Text;
+            passwordEntry.Placeholder = "Password";
+            stackLayout.Children.Add(passwordEntry);
+
+            createBtn = new Button();
+            createBtn.Text = "Create Account";
+            createBtn.Clicked += createBtn_Clicked;
+            stackLayout.Children.Add(createBtn);
+        }
+
+        private void createBtn_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
