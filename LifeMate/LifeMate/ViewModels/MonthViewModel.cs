e8b9b3ddfe0f3289a92e4eda8f5f6ff5158b72de
IAB330Team6 <iab330team6@gmail.com>
2019-11-04 15:02:09 +1000

    Month View Events Display Fixed

---
+++ LifeMate/LifeMate/ViewModels/MonthViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeMate.ViewModels
+{
+    public class MonthViewModel : EventModelBase
+    {
+        public MonthViewModel()
+        {
+
+        }
+    }
+}
